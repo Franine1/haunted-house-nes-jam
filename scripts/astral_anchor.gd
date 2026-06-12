0e128903cada081211c54309fa8f3efd8d0a6ca7
Franine1 <franen321divinenergy@gmail.com>
2026-06-12 13:48:31 -0400

    additional rooms in second floor astral plane

--- scripts/astral_anchor.gd
+++ scripts/astral_anchor.gd
@@ -1,7 +1,11 @@
 ## An astral zone capable of teleporting the inactive player either
 ## Upon the x axis, y axis, both, or neither. You can also choose whether
 ## It accepts data from the active player's position, which can be used
-## To offset the result position if both the enforce and copy booleans are true.
+## to offset the result position if both the enforce and copy booleans are true.
+## ENFORCE: sets an axis to the chosen value
+## COPY: sets an axis to the value of the active player
+## BOTH: sets an axis to the value of the active player, then offsets it by the chosen value
+## NEITHER: retain the original value of this axis
 class_name AstralAnchor
 extends AstralZone
 
@@ -19,7 +23,7 @@ extends AstralZone
 
 
 
-func enforce_distances(player_container: PlayerContainer, active_player: Player, _inactive_player: Player):
+func enforce_distances(player_container: PlayerContainer, active_player: Player, inactive_player: Player):
 	var enforce_axis: Vector2 = Vector2.ZERO
 	if enforce_X:
 		enforce_axis += Vector2.RIGHT
@@ -32,12 +36,23 @@ func enforce_distances(player_container: PlayerContainer, active_player: Player,
 	if copy_Y:
 		copy_axis -= Vector2.DOWN
 	
+	var retain_axis: Vector2 = Vector2.ONE
+	if enforce_X or copy_X:
+		retain_axis -= Vector2.RIGHT
+	if enforce_Y or copy_Y:
+		retain_axis -= Vector2.DOWN
+	
 	var source_pos: Vector2 = active_player.final_position() / 16.0
+	
+	var retained_pos: Vector2 = inactive_player.final_position() / 16.0
+	retained_pos -= source_pos
 	
 	var enforced_vector: Vector2 = Vector2(target_screen) + (Vector2(target_tile) / screen)
 	
 	var unchanged_vector: Vector2 = source_pos / screen
 	
-	var final: Vector2 = (enforce_axis * enforced_vector) - (copy_axis * unchanged_vector)
+	var retained_vector: Vector2 = retained_pos / screen
+	
+	var final: Vector2 = (enforce_axis * enforced_vector) - (copy_axis * unchanged_vector) + (retain_axis * retained_vector)
 	
 	player_container.adjust_player_distances(final)
